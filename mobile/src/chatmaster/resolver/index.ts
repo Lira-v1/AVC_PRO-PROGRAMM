d6b67d5d12a9ac1cc1070954a036b8f2427daa80
Lira-v1 <sakeev.ilyas@gmail.com>
2026-03-10 18:08:24 +0500

    Add categorized request phrase dictionary for resolver

--- mobile/src/chatmaster/resolver/index.ts
+++ mobile/src/chatmaster/resolver/index.ts
@@ -1,5 +1,6 @@
 export { WorkRequestResolver } from './workRequestResolver';
 export { requestDictionary } from './requestDictionary';
+export { requestPhrasesByCategory } from './requestPhrasesByCategory';
 export { extractParams } from './parameterExtractor';
 export { REQUEST_PATTERNS, UNIT_NORMALIZATION } from './requestPatterns';
 export type { WorkRequestResult, DictionaryEntry, ExtractedParams } from './types';
